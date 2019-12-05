bacd6df48ff94b3cef3c49cbc6045b1d0fe1421c
Evgeniy Kvach <Evgeniy.Kvach@newdv.ru>
2019-12-05 14:24:50 +0300

    fix small bug

--- example/App.js
+++ example/App.js
@@ -2,7 +2,7 @@
 angular.module('App', [])
 .controller('MyAppCtrl', ['$scope',
 	($scope) => {
-		$scope.componentTitle ='Страничка для тестирования Galinka state manager';
+		$scope.componentTitle ='Компонент1';
 		const ctrl1Galinka = new Galinka();
 		const module1State = ctrl1Galinka.getState('module1');
 		const module2State = ctrl1Galinka.getState('module2');
@@ -22,7 +22,7 @@ angular.module('App', [])
 	}])
 	.controller('MyAppCtrl2', ['$scope',
 	($scope) => {
-		$scope.componentTitle ='Страничка для тестирования Galinka state manager';
+		$scope.componentTitle ='Компонент2';
 		const ctrl2Galinka = new Galinka();
 		$scope.allAppState = ctrl2Galinka.getState();
 	}]);
