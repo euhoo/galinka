10cdbc7f5d4c76857679b7da635e3fdf301074a1
euhoo <euhoo@mail.ru>
2019-12-05 22:49:51 +0300

    trying add stores

--- galinka.js
+++ galinka.js
@@ -1,9 +1,15 @@
 class Galinka {
+
+	constructor(storeConstructors) {
+		this.storeConstructors = storeConstructors;
+	}
+
 	initState = () => {
 		if(!this.__proto__.Galinka) this.__proto__.Galinka = {};
 		if(!this.__proto__.GalinkaHistory) this.__proto__.GalinkaHistory = [];
 		this.__proto__.isGalinkaInited = true;
 	}
+
 	setState = (state, data) => {
 		if(!this.__proto__.isGalinkaInited) this.initState();
 		const oldState = this.__proto__.Galinka[state] ? this.__proto__.Galinka[state] : {};
@@ -11,11 +17,29 @@ class Galinka {
 		this.__proto__.Galinka[state] = this.__proto__.Galinka[state] ? {...this.__proto__.Galinka[state], ...data} : { ...data };
 		//добавлять здесь историю после того как корректно буду добавлять обновлять state
 	}
+
 	getState = (state) => {
 		if(!this.__proto__.isGalinkaInited) return console.error('Galinka have not been inited and there is no state here. Please use Galinka.setState() before')
 		if(!state) return this.__proto__.Galinka;
 		return this.__proto__.Galinka[state];
 	}
+
+	setStore = (type, newStore) => {
+		const storeConstructor = this.storeConstructors.find(constructor => {
+			constructor.type === type;
+		});
+		const updatedStore = storeConstructor(newStore);
+		const updatedStores = this.__protp__.GalinkaStores.map(item => {
+			if(item.type === type) return updatedStore;
+			return item;
+		});
+		this.__proto__.GalinkaStores = updatedStores;
+	}
+
+	setStoreConstructor = (type, storeConstructor) => {
+		//прописать добавление конструкторов store по типу
+	}
+
 	addHistory = (currentAppState) => {
 		this.__proto__.GalinkaHistory = [...this.__proto__.GalinkaHistory, currentAppState];
 	}
