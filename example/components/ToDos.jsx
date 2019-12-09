d0c4f577e0264b74860096a4927244b14cc39172
euhoo <euhoo@mail.ru>
2019-12-09 21:37:00 +0300

    works but badly

--- example/components/ToDos.jsx
+++ example/components/ToDos.jsx
@@ -4,8 +4,25 @@ import Galinka from '../../galinka';
 const inputStore = new Galinka('toDos');
 
 export default class ToDos extends Component {
+    state = {
+        data:'',
+    }
+    componentDidMount = () => {
+        const id = setInterval(() => {
+            const data = inputStore.getStore();
+            if(data) this.setState({ data });
+        }, 100);
+        this.setState({ id });
+    }
+
+    componentWillUnmount = () => {
+        const { id } = this.state;
+        clearInterval(id);
+    }
+
     render() {
-        const data = inputStore.getStore();
+       const { data } = this.state;
+        
         const toDos = data ? data.map(item => <div key={item.id}>{item.data}</div>) : null;
         return (
             <div>
