208917dc1542a98637e50b59c8140280de883b63
euhoo <euhoo@mail.ru>
2019-12-21 15:05:34 +0300

    update update func, make better docs

--- example/components/ToDos.jsx
+++ example/components/ToDos.jsx
@@ -3,17 +3,13 @@ import Galinka from '../../galinka';
 
 const inputStore = new Galinka('toDos');
 
-export default class ToDos  extends Component {
+export default class ToDos extends Component {
     state = {
         data: '',
     };
     componentDidMount = () => {
-        const stateFunc = () => {
-            this.setState({ demoValue: '' })
-        };
-        inputStore.addStateFunc(stateFunc, 'toDos');
+        inputStore.addStateFunc(this.setState, 'toDos');
     };
-
 
     render() {
         console.log('rendering TODOS');
